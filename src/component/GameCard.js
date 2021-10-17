da12e7be2995abe38d04cc0e8ea86a9dc12d752f
Jerem <jerem.blcn@gmail.com>
2021-10-17 21:14:25 +0200

    tc

--- src/component/GameCard.js
+++ src/component/GameCard.js
@@ -10,24 +10,11 @@ function GameCard(props){
                     loading="lazy"
                 />
                 <div className="flex justify-between -mt-4 px-4">
-                    <span className="inline-block ring-4 bg-red-500 ring-gray-800 rounded-full text-sm font-medium tracking-wide text-gray-100 px-3 pt-0.5">
+                    <span className="inline-block ring-4 bg-red-500 ring-gray-800 rounded-full text-sm font-medium tracking-wide text-gray-100 px-3 py-0.5">
                         {props.category}
                     </span>
-                    <span className="flex h-min space-x-1 items-center rounded-full text-gray-400 bg-gray-800 py-1 px-2 text-xs font-medium">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5 text-blue-500"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                            />
-                        </svg>
-                        <p className="text-blue-500 font-semibold text-xs">
-                            {props.console}
-                        </p>
+                    <span className="flex h-min space-x-1 items-center rounded-full text-gray-100 bg-red-500 ring-4 ring-gray-800 px-3 py-0.5 text-xs font-medium">
+                        {props.console}
                     </span>
                 </div>
                 <div className="py-2 px-4">
@@ -35,7 +22,7 @@ function GameCard(props){
                         <a href="blog/detail">{props.title}</a>
                     </h1>
                 </div>
-                <div className="px-4 space-y-2">
+                <div className="px-4 pb-4 space-y-2">
                     <p className="text-gray-400 font-normal leading-5 tracking-wide">
                         {props.description}
                     </p>
